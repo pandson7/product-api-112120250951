7a7af71964f084564c96c021af1582e84fca85c2
pandson7 <pandson@amazon.com>
2025-11-21 10:12:54 -0500

    Add getProductById Lambda function

---
+++ src/lambda/getProductById.js
@@ -0,0 +1,80 @@
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient, GetCommand } = require('@aws-sdk/lib-dynamodb');
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+exports.handler = async (event) => {
+  console.log('Event:', JSON.stringify(event, null, 2));
+  
+  try {
+    const productId = event.pathParameters?.id;
+    
+    if (!productId) {
+      return {
+        statusCode: 400,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({
+          error: {
+            code: 'INVALID_REQUEST',
+            message: 'Product ID is required',
+          },
+        }),
+      };
+    }
+
+    const command = new GetCommand({
+      TableName: process.env.TABLE_NAME,
+      Key: {
+        productId: productId,
+      },
+    });
+
+    const result = await docClient.send(command);
+    
+    if (!result.Item) {
+      return {
+        statusCode: 404,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({
+          error: {
+            code: 'NOT_FOUND',
+            message: 'Product not found',
+          },
+        }),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, X-Amz-Date, Authorization, X-Api-Key',
+      },
+      body: JSON.stringify(result.Item),
+    };
+  } catch (error) {
+    console.error('Error:', error);
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        error: {
+          code: 'INTERNAL_ERROR',
+          message: 'Internal server error',
+        },
+      }),
+    };
+  }
+};
